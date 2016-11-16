bcaedd07995e8974d0a2f843c0a51b2f9d01527a
VanDeng95 <249904130@qq.com>
2016-11-16 21:10:09 +0800

    Add filter

--- onHand/Src/softconv.c
+++ onHand/Src/softconv.c
@@ -2,25 +2,60 @@
 
 #include "gpio.h"
 
-void get_ADC(uint8_t ChNumber, uint32_t ADC_ConvertedValue[], uint16_t ADC_ConvertedValue_AfterMaping[]){
+uint32_t ADC_ConvertedValue[5];
+float ADC_ConvertedValue_AfterMapping[10];
+float ADC_ConvertedValue_Average[10];
+
+
+uint32_t *get_ADC(uint8_t ChNumber){
 
 //HAL_ADCEx_MultiModeConfigChannel(&hadc1, hadc2);  //Optionally,for devices with several ADC instances
-  uint8_t slope = 0;
-	uint8_t offset =0;
-	uint8_t i = 0;
+ 
 	
 	HAL_ADC_Start(&hadc2);//  Activate the ADC peripheral (slave) and start conversions
 	HAL_ADCEx_MultiModeStart_DMA(&hadc1,ADC_ConvertedValue,ChNumber);//Activate the ADC peripheral (master) and start conversions
 	
-	for(i,i<5,i++){
-		ADC_ConvertedValue_AfterMaping[2*i] = slope*(ADC_ConvertedValue[i]>>16)+offset;
-		ADC_ConvertedValue_AfterMaping[2*i+1] = slope*((uint16_t)(ADC_ConvertedValue[i]&0xffff))+offset;
-	}
+	return ADC_ConvertedValue;
 
-	
-	
 
 	//Conversion results are automatically transferred by DMA into destination variable address.	
 	//HAL_ADCEx_MultiModeStop_DMA() //Stop conversion and disable the ADC peripheral (master)
 	//HAL_ADC_Stop_IT()//Stop conversion and disable the ADC peripheral (master)
+}
+
+float *mapping_ADC(float maxvalue, float minvalue,uint8_t ChNumber){
+	  get_ADC(ChNumber);
+		float slope = 90/(maxvalue-minvalue);
+		float offset =90*minvalue/(minvalue-maxvalue);
+		uint8_t i = 0;
+		for(i,i<5,i++){
+			ADC_ConvertedValue_AfterMapping[2*i] = slope*(ADC_ConvertedValue[i]>>16)+offset;
+			ADC_ConvertedValue_AfterMapping[2*i+1] = slope*((uint16_t)(ADC_ConvertedValue[i]&0xffff))+offset;
+		}
+
+		return ADC_ConvertedValue_AfterMapping;
+}
+
+
+
+float *get_ADC_AverageValue(uint16_t times,uint16_t maxvalue, uint16_t minvalue, uint8_t ChNumber){
+		uint8_t i = 0;
+		uint8_t m = 0;
+		uint8_t weight = 0;
+		mapping_ADC(maxvalue,minvalue,ChNumber);
+	  *ADC_ConvertedValue_Average = *ADC_ConvertedValue_AfterMapping;
+		for(i;i<times-1;i++){
+				mapping_ADC(maxvalue,minvalue,ChNumber);
+				for(m;m<10;m++)
+					ADC_ConvertedValue_Average[m]=weight*ADC_ConvertedValue_Average[i]+(1-weight)*ADC_ConvertedValue_AfterMapping[i];			
+		}	
+		return ADC_ConvertedValue_Average;
+}
+
+
+
+void ADC_print(){
+	uint8_t i = 0;
+	for(i;i<10;i++)
+		printf("Channel,%f, %f",i+1,ADC_ConvertedValue_Average[i]);
 }
--- onHand/Src/stm32f1xx_it.c
+++ onHand/Src/stm32f1xx_it.c
@@ -91,6 +91,7 @@ void SysTick_Handler(void)
 */
 void DMA1_Channel1_IRQHandler(void)
 {
+	
   /* USER CODE BEGIN DMA1_Channel1_IRQn 0 */
 
   /* USER CODE END DMA1_Channel1_IRQn 0 */
